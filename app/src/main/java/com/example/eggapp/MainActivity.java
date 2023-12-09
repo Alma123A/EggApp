4dacd545e14994499660ccb5c752a4ee4fc89c4a
Alma123A <alma.tisch1@gmail.com>
2023-12-09 17:01:29 +0200

    add green egg

---
+++ app/src/main/java/com/example/eggapp/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.eggapp;
+
+import android.os.Bundle;
+import android.widget.TextView;
+import android.view.View;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private int counter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        counter = 10;
+        View counterTextView = findViewById(R.id.Textview_counter);
+        counterTextView.setText("" + counter);
+
+    }
+
+    public void knock(View view) {
+        if (counter > 0) {
+            counter--;
+            View counterTextView = findViewById(R.id.Textview_counter);
+            counterTextView.setText("" + counter);
+            if (counter == 0) {
+                ImageView eggImageView = findViewById(R.id.ImageView_egg);
+                eggImageView.setImageResource(R.drawable.suprize);
+            }
+
+        }
+    }
+
+    public void reset(View view) {
+        counter = 10;
+        View counterTextView = findViewById(R.id.Textview_counter);
+        counterTextView.setText()
+        ImageView eggImageView = findViewById(R.id.ImageView_egg);
+        eggImageView.setImageResource(R.drawable.egg);
+    }
+}
---
+++ app/src/main/java/com/example/eggapp/Textview.java
@@ -0,0 +1,6 @@
+package com.example.eggapp;
+
+public class Textview {
+    public void setText(String s) {
+    }
+}
